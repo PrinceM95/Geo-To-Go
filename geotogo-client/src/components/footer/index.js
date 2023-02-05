4adaf5faac8dd8807abc2daf65190e882a732235
PrinceM95 <moutimap@gmail.com>
2023-02-05 14:17:02 -0500

    mock homepage created

--- geotogo-client/src/components/footer/index.js
+++ geotogo-client/src/components/footer/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import style from './style.module.scss';
 
 const Footer = () => (
-    <div className={style.footer}></div>
-
-
+    <div className={style.footer}>
+        <div>&copy; WCCI 2023</div>
+    </div>
 );
     
 export default Footer;
