3ac17c6b610705aff93c00bcd832da4fa226b49c
PrinceM95 <moutimap@gmail.com>
2023-02-08 20:12:11 -0500

    added stuff to home and africa page

--- geotogo-client/src/pages/home/index.js
+++ geotogo-client/src/pages/home/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import style from './style.module.scss';
+import Continents from '../../components/Continents.js';
 
 const HomePage = () => {
 
     return (
         <div className={style.home}>
-        <h1>Home page!</h1>
+            {/* <Continents/> */}
         </div>
         ); 
     }
