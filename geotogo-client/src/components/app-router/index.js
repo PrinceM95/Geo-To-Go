b3512446a15ea308979fdf0aa013913c2251f924
conniecon <littlemissconnie@yahoo.com>
2023-02-18 18:38:40 -0500

    fixed about classNames

--- geotogo-client/src/components/app-router/index.js
+++ geotogo-client/src/components/app-router/index.js
@@ -2,14 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import AboutPage from '../../pages/about';
 import AfricaPage from '../../pages/africa';
+import AfricaQPage from '../../pages/africa_quiz';
 import AsiaPage from '../../pages/asia';
-import ContactPage from '../../pages/contact';
+// import ContactPage from '../../pages/contact';
 import EuropePage from '../../pages/europe';
 // import LandingPage from '../../pages/landing';
 import HomePage from '../../pages/home';
 import NAPage from '../../pages/na';
 import OceaniaPage from '../../pages/oceania';
-import AfricaQPage from '../../pages/africa_quiz';
 import React from 'react';
 import SAPage from '../../pages/sa';
 
@@ -23,10 +23,10 @@ const PageSwitch = () => (
         <Route exact path={'/north america'} element={<NAPage />} />
         <Route exact path={'/south america'} element={<SAPage />} />
         <Route exact path={'/oceania'} element={<OceaniaPage />} />
-        <Route exact path={'/contact'} element={<ContactPage />} />
+        {/* <Route exact path={'/contact'} element={<ContactPage />} /> */}
         <Route exact path={'/about'} element={<AboutPage />} />
         <Route exact path={'/africa_quiz'} element={<AfricaQPage />} />
-        <Route exact path={'/landing'} element={<LandingPage />} />
+        {/* <Route exact path={'/landing'} element={<LandingPage />} /> */}
     </Routes>
 );
 
--- geotogo-client/src/pages/about/index.js
+++ geotogo-client/src/pages/about/index.js
@@ -9,29 +9,29 @@ const AboutPage = () => {
 
     return (
         <div className={style.about}>
-            <div className={style.about-heading}>
+            <div className={style.aboutHeading}>
                 <h1>Geo-To-Go Development Team</h1>  
             </div>
-            <div className={style.about-container}>
-                <div className={style.about-item}>
+            <div className={style.aboutContainer}>
+                <div className={style.aboutItem}>
                     <h2>Constance Cain</h2>
-                    <img className={style.about-img} src={constImg} alt="constance-profile" width='300' height='auto' />
-                    <p className={style.about-bio}>I live in Columbus, Ohio. I am a student developer at We Can Code IT. My hobbies include Photography and Painting</p>  
+                    <img className={style.aboutImage} src={constImg} alt="constance-profile" width='300' height='auto' />
+                    <p className={style.aboutBio}>I live in Columbus, Ohio. I am a student developer at We Can Code IT. My hobbies include Photography and Painting</p>  
                 </div>
-                <div className={style.about-item}>
+                <div className={style.aboutItem}>
                     <h2>Kadeen Jennings</h2>
-                    <img className={style.about-img} src={kadeenImg} alt="kadeen-profile" width='300' height='auto' />
-                    <p className={style.about-bio}>I live in Columbus, Ohio. I am a student developer at We Can Code IT. My hobbies include Photography and Painting</p>  
+                    <img className={style.aboutImage} src={kadeenImg} alt="kadeen-profile" width='300' height='auto' />
+                    <p className={style.aboutBio}>I live in Columbus, Ohio. I am a student developer at We Can Code IT. My hobbies include Photography and Painting</p>  
                 </div>
-                <div className={style.about-item}>
+                <div className={style.aboutItem}>
                     <h2>Doug Holt</h2>
-                    <img className={style.about-img} src={dougImg} alt="doug-profile" width='300' height='auto' />
-                    <p className={style.about-bio}>I live in Columbus, Ohio. I am a student developer at We Can Code IT. My hobbies include Professional Rodeo Clown and eating green Jello</p>  
+                    <img className={style.aboutImage} src={dougImg} alt="doug-profile" width='300' height='auto' />
+                    <p className={style.aboutBio}>I live in Columbus, Ohio. I am a student developer at We Can Code IT. My hobbies include Professional Rodeo Clown and eating green Jello</p>  
                 </div>
-                <div className={style.about-item}>
+                <div className={style.aboutItem}>
                     <h2>Prince Moutima</h2>
-                    <img className={style.about-img} src={princeImg} alt="prince-profile" width='300' height='auto' />
-                    <p className={style.about-bio}>I live in Columbus, Ohio. I am a student developer at We Can Code IT. My hobbies include Photography and Painting</p>  
+                    <img className={style.aboutImage} src={princeImg} alt="prince-profile" width='300' height='auto' />
+                    <p className={style.aboutBio}>I live in Columbus, Ohio. I am a student developer at We Can Code IT. My hobbies include Photography and Painting</p>  
                 </div>
             </div>
         </div>
