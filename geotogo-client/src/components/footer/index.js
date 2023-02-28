6ef5ea338d20190f0c235769d2b72008964ce7e6
conniecon <littlemissconnie@yahoo.com>
2023-02-28 09:10:28 -0500

    footer styles with social icons

--- geotogo-client/src/components/footer/index.js
+++ geotogo-client/src/components/footer/index.js
@@ -1,32 +1,35 @@
 import React from "react";
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
 import style from './style.module.scss';
 
 const Footer = () => ( 
 
     <div className={style.footer}>
-        <div>
-            {/* <h3>Contact Us</h3>   */}
-        </div>
+        {/* <div>
+            <h3>Contact Us</h3>
+        </div> */}
         <div>
             <h3>&copy; Geo-To-Go 2023</h3>
         </div>
-        {/* <div className={style.socialList}> */}
-            
-                {/* <a><SocialIcon url="https://github.com/2022-October-Java-PT/GeoToGo" /></a>
-                <a><SocialIcon url="https://linkedin.com" /></a>
-                <a><SocialIcon url="https://facebook.com" /></a>
-                <a><SocialIcon url="https://instagram.com" /></a> */}
+        <div className={style.socialList}> 
+                
+                <SocialIcon style={{ height: 40, width: 40, margin: 5 }}  bgColor="white" url="https://github.com/2022-October-Java-PT/GeoToGo" target="_blank" rel="noopener noreferrer"/>
+                
+                <SocialIcon style={{ height: 40, width: 40, margin: 5 }} bgColor="white" url="https://linkedin.com" target="_blank" rel="noopener noreferrer"/>
+                
+                <SocialIcon style={{ height: 40, width: 40, margin: 5 }} bgColor="white" url="https://facebook.com" target="_blank" rel="noopener noreferrer"/>
+                
+                <SocialIcon style={{ height: 40, width: 40, margin: 5 }} bgColor="white" url="https://instagram.com" target="_blank" rel="noopener noreferrer"/>
                 
             
         
                 
                 
-                {/* <li><a href="https://github.com/2022-October-Java-PT/digi-dreamers-mystery-educator-repo" target="_blank" rel="noopener noreferrer">GitHub</a></li>
+                {/* <li><a href="https://github.com/2022-October-Java-PT/GeoToGo" target="_blank" rel="noopener noreferrer">GitHub</a></li>
                 <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a></li>
-                <li><a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li> */} 
-        {/* </div> */}
+                <li><a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></li>  */}
+        </div>
 
     </div>
 );
