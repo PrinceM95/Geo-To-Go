b976ff19213c0e24513610220342f8cd9dbd2cb5
PrinceM95 <moutimap@gmail.com>
2023-03-01 19:48:59 -0500

    poopity scoop

--- geotogo-client/src/pages/landing-page/index.js
+++ geotogo-client/src/pages/landing-page/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
 import style from './style.module.scss';
 import world from '../../assets/world4.mp4';
 
@@ -8,7 +7,7 @@ const LandingPage = () => {
 
     return (
         <div className={style.landing}>
-            <div className={style.button}><Link to="/home" target="_blank" rel="noreferrer">Enter</Link></div>
+            <div className={style.button}></div>
         <div className={style.opener}> “Your one-stop shop for continents, countries, capitals and cultures at their core. We are committed to providing a positive learning environment that inspires humanity, both in the air and on the ground. This is Geo-To-Go”
         </div>
         <video  autoPlay muted loop width="100%" height="30%">
